d2bb6d1c827521bddac8237da6bd9a648d52a552
Kpavicic00 <kpavic00@fesb.hr>
2019-03-22 19:49:57 +0100

    git pusg

--- Meni.py
+++ Meni.py
@@ -1,1 +1,11 @@
-print(432)
+import numpy as np
+from functions import *
+
+# txt FILES
+file_txt = "file.txt"
+
+
+# varijable nizovi numpay
+# printFile(file_txt)
+print("\n\t Index")
+print("\n\t Index")
