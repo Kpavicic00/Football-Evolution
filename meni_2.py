98abcd53d24556f18f5dbe424dd921c0636e165c
Kpavicic00 <kpavic00@fesb.hr>
2019-03-23 13:42:44 +0100

    Initial Commit

---
+++ meni_2.py
@@ -0,0 +1,23 @@
+import numpy as np
+from functions import *
+
+
+
+# txt FILES
+koef = "/home/kristijan/github/FootballEvolcion/file.txt"
+data = "/home/kristijan/github/FootballEvolcion/datas.txt"
+
+print("\n\t Ispis koeficeijanata : ")
+printFile(koef)
+
+print("\n\t Ispis podataka : ")
+printFile(data)
+
+#print("\n\t Duljina redaka u Koef : "file_lengthy(koef),"\n\t Duljina redaka u podacima : "file_lengthy(data))
+
+un = "0e"
+print("\n\t Uneis   :   ")
+un = raw_input("Uneis nes:")
+print(un)
+
+#rez = Koef_suma_umozak(koef,un)
