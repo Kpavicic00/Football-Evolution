8e0de15e2b23acbb91ce771bbbaaf2a8b34540d7
Kpavicic00 <kpavic00@fesb.hr>
2019-03-26 08:39:33 +0100

    Initial Commit

--- Meni.py
+++ Meni.py
@@ -1,30 +1,34 @@
+# -*- coding: utf-8 -*-
 import numpy as np
+import pandas as pd
+import csv
+import sys
+from pandas import ExcelWriter
+from pandas import ExcelFile
 from functions import *
+import sys
+
 
 
 
 # txt FILES
+data_csv = "datas.csv"
 koef = "/home/kristijan/github/FootballEvolcion/file.txt"
 data = "/home/kristijan/github/FootballEvolcion/datas.txt"
 
-print("\n\t Ispis koeficeijanata : ")
-printFile(koef)
+# print("\n\t Ispis koeficeijanata : ")
+# printFile(koef)
+#
+# print("\n\t Ispis podataka : ")
+# printFile(data)
 
-print("\n\t Ispis podataka : ")
-printFile(data)
+dat = "/home/kristijan/github/FootballEvolcion/datas.csv"
 
-
-# deklaracija varijabli i incijalizacija
-
-un = 0.0
-string = "0.0"
-
-# pozivi funkcija
-
-un = Coefficients(koef)
-string = str(un)
-
-
-print("\n\t Koeficijent inflacije za tu godinu prema  doansnjoj vrijednosti : ",un)
-
-#print("\n\t Duljina redaka u Koef : "file_lengthy(koef),"\n\t Duljina redaka u podacima : "file_lengthy(data))
+# data = pd.read_csv('/home/kristijan/github/FootballEvolcion/datas.csv', delimiter = ','
+# ,names = ['poredak\t','Naziv lige','potrošeno','dolso ugraca','doslo igraca','otislo igraca'])
+# print(data)
+data = pd.read_csv(dat, names=['poredak', 'b', 'c','d','e','g','g'])
+print(data)
+shap = data.shape
+print(shap)
+data.loc['1':'2',['b','c','d']]
