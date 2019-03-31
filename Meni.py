6a22e2996872b8e09d3ef743e91524aa5dea998b
Kpavicic00 <kpavic00@fesb.hr>
2019-03-31 16:01:37 +0200

    Initial Commit

--- Meni.py
+++ Meni.py
@@ -12,61 +12,13 @@ import sys
 
 
 # txt FILES
-data_csv = "income_data.csv"
+podaci = "/home/kristijan/github/FootballEvolcion/TEST_CSV.csv"
 koef = "/home/kristijan/github/FootballEvolcion/file.txt"
 data = "/home/kristijan/github/FootballEvolcion/datas.txt"
+podaci = "/home/kristijan/github/FootballEvolcion/podaci.txt"
 
 # print(df1.index.tolist()) #print index
 
 
-data = pd.read_csv(data_csv)
-#print(data)
-
-#t = df2.loc["Alaska":"Arkansas","2005":"2007"]
-print("\n\t test : ")
-
-print(data.head())
-# Type_new = pd.Series([])
-# data.insert(2, "Type New", Type_new) ### umetanje novih stupaca colums
-usp = "Sta"
-test = "jebemu"
-i =1
-t = input("Unesi : ")
-if i == t:
-    data [usp][0] = test
-    print("Ipsis : ")
-    print(data.head())
-
-
-#print(data.head())
-
-#print(data.head())
-# t = input("pocija 1 : ")
-# t = int(t)
-# a =1
-# if a == t:
-#     b= "jebemu"
-#     b = str(b)
-#     if data.loc["Type New",0]  == b:
-#         print(" Ispis : ",data.loc["Type New",0])
-
-# df['proxyCity'].ix[myindex ] = new_name
-# to
-#
-# df.loc[myindex, 'proxyCity'] = new_name
-#unos = input("\n\t Unesit etesnu rijec : ")
-
-
-#data["Type New"][1] = unos
-# for i in range(len(data)):
-#     if data["Type New"][i] == "NaN":
-#         Type_new[i]="Green"
-#
-#     elif data["Type New"][i] == "NaN":
-#         Type_new[i]="Orange"
-#
-#     elif data["Type New"][i] == "NaNr":
-#         Type_new[i]="Blue"
-#
-#     else:
-#         Type_new[i]= data["Type New"][i]
+dat = pd.read_csv(podaci)
+print(dat)
