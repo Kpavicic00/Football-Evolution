eb5cb9f95ff46a46d633f641a3c33e5bd03014ce
Kpavicic00 <kpavic00@fesb.hr>
2019-05-04 12:14:17 +0200

    Initial Commit

--- files_lib.py
+++ files_lib.py
@@ -1,19 +1,46 @@
 
-
 # csv , txt files for datas
 clubs_KONACNA = '/home/kristijan/github/FootballEvolcion/Datas/sportska_kubska_statsitika_OBRDENO.csv' # ovaj za klubsku statistiku
 fp_league = '/home/kristijan/github/FootballEvolcion/Datas/Ligaska_KONACAN_STAS.csv'# ovaj za ligasku statistiku
 
 
 # csv file for save of processed data
-save_csv_Expend = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_a.csv'
-save_csv_Expend_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_b.csv'
 
-save_csv_Income = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_c.csv'
-save_csv_Income_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_d.csv'
+#   LEAUGE csv files for save processed datas from functions for LEAUGE statistic
+# EXPEND  and BATCH for EXPEND
+save_csv_Expend = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_Expend.csv'
+save_csv_Expend_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_Expend_BATCH.csv'
+###############################################################################
 
-save_csv_Balance = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_e.csv'
-save_csv_Balance_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_f.csv'
+# INCOME  and BATCH for INCOME
+save_csv_Income = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_Income.csv'
+save_csv_Income_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_Income_BATCH.csv'
+###############################################################################
 
-save_csv_GetDataForLeauge_AVG_Seasons = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_g.csv'
-save_csv_GetDataForLeauge_AVG_Seasons_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_h.csv'
+# BALANCE  and BATCH for BALANCE
+save_csv_Balance = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_Balance.csv'
+save_csv_Balance_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_Balance_BATCH.csv'
+###############################################################################
+
+# GetDataForLeauge_AVG_Seasons  and BATCH for GetDataForLeauge_AVG_Seasons
+save_csv_GetDataForLeauge_AVG_Seasons = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_GetDataForLeauge_AVG_Seasons.csv'
+save_csv_GetDataForLeauge_AVG_Seasons_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_GetDataForLeauge_AVG_Seasons_BATCH.csv'
+###############################################################################
+
+# GetBYyear  and BATCH for GetBYyear
+save_csv_GetBYyear = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_GetBYyear.csv'
+save_csv_GetBYyear_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_GetBYyear_BATCH.csv'
+#######################################################################################################################################
+
+
+#   CLUBS csv files for save processed datas from functions for clubs statistic
+# GETDataClubs_with_season  and BATCH for GETDataClubs_with_season
+save_csv_GETDataClubs_with_seasons = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_GETDataClubs_with_seasons.csv'
+save_csv_GETDataClubs_with_seasons_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_GETDataClubs_with_seasons_BATCH.csv'
+###############################################################################
+
+# GetDate_for_Clubs_throught_all_seasons  and BATCH for GetDate_for_Clubs_throught_all_seasons
+save_csv_GetDate_for_Clubs_throught_all_seasons = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_GetDate_for_Clubs_throught_all_seasons.csv'
+save_csv_GetDate_for_Clubs_throught_all_seasons_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/save_csv_GetDate_for_Clubs_throught_all_seasons_BATCH.csv'
+###############################################################################
+#######################################################################################################################################
