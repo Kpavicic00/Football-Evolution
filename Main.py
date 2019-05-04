3e92fc6b49bbd36620e640ae11a16b7030c1d656
Kpavicic00 <kpavic00@fesb.hr>
2019-05-04 11:53:53 +0200

    Initial Commit

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import csv
 import sys
+from files_lib import *
 from pandas import ExcelWriter
 from pandas import ExcelFile
 from collections import Counter
@@ -15,24 +16,6 @@ from sort_functions import*
 
 
 
-# csv , txt files for datas
-clubs_KONACNA = '/home/kristijan/github/FootballEvolcion/Datas/sportska_kubska_statsitika_OBRDENO.csv' # ovaj za klubsku statistiku
-fp_league = '/home/kristijan/github/FootballEvolcion/Datas/Ligaska_KONACAN_STAS.csv'# ovaj za ligasku statistiku
-test = '/home/kristijan/github/FootballEvolcion/Datas/testni_podaci.csv'
-
-koef = "/home/kristijan/github/FootballEvolcion/Datas/file.txt"
-data = "/home/kristijan/github/FootballEvolcion/Datas/datas.txt"
-podaci = "/home/kristijan/github/FootballEvolcion/Datas/podaci.txt"
-
-# csv file for save of processed data
-save_csv_a = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_a.csv'
-save_csv_b = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_b.csv'
-save_csv_c = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_c.csv'
-save_csv_d = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_d.csv'
-save_csv_e = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_e.csv'
-save_csv_f = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_f.csv'
-save_csv_g = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_g.csv'
-save_csv_h = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_h.csv'
 
 clubs_DF = DataFrameFunc(fp_league)
 BATCH_for_GetAVGExpendFORplayerArrivals(clubs_DF)
---
+++ files_lib.py
@@ -0,0 +1,19 @@
+
+
+# csv , txt files for datas
+clubs_KONACNA = '/home/kristijan/github/FootballEvolcion/Datas/sportska_kubska_statsitika_OBRDENO.csv' # ovaj za klubsku statistiku
+fp_league = '/home/kristijan/github/FootballEvolcion/Datas/Ligaska_KONACAN_STAS.csv'# ovaj za ligasku statistiku
+
+
+# csv file for save of processed data
+save_csv_Expend = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_a.csv'
+save_csv_Expend_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_b.csv'
+
+save_csv_Income = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_c.csv'
+save_csv_Income_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_d.csv'
+
+save_csv_Balance = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_e.csv'
+save_csv_Balance_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_f.csv'
+
+save_csv_GetDataForLeauge_AVG_Seasons = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_g.csv'
+save_csv_GetDataForLeauge_AVG_Seasons_BATCH = '/home/kristijan/github/FootballEvolcion/Datas/SaveData/Save_csv_Parsing_h.csv'
